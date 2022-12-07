7706d1d2d83b1ebd90cfdc369b050e71b23206bf
puffingprie <morte1916@gmail.com>
2022-12-07 07:51:30 -0600

    1

--- MCSAllNPCsSpawn/MCSAllNPCsSpawn.cs
+++ MCSAllNPCsSpawn/MCSAllNPCsSpawn.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Bag;
 using BepInEx;
+using Fungus;
+using GUIPackage;
+using HarmonyLib;
+using JiaoYi;
+using JSONClass;
+using KBEngine;
+using KillSystem;
+using Newtonsoft.Json.Linq;
+using PaiMai;
+using script.KillSystem;
+using Tab;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using YSGame.TianJiDaBi;
 
 namespace MCSAllNPCsSpawn
 {
@@ -13,6 +28,33 @@ namespace MCSAllNPCsSpawn
         public const string pluginGuid = "arx.mcs.allnpcsspawn";
         public const string pluginName = "MCSAllNPCsSpawn";
         public const string pluginVersion = "1.0.0.0";
-        void Start() { }
+
+        private static void UselessDebuggerPrefix()
+        {
+            Debug.Log("===================MCSAllNPCsSpawn=====================");
+        }
+        private void Awake()
+        {
+            UselessDebuggerPrefix();
+            Debug.Log("MCSAllNPCsSpawn Loaded");
+        }
+        private void Start()
+        {
+            UselessDebuggerPrefix();
+            Debug.Log("MCSAlNPCsSpawn: Game started...");
+            Harmony.CreateAndPatchAll(typeof(MCSAllNPCsSpawn), null);
+            MCSAllNPCsSpawn.Inst = this;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(NPCFactory), "firstCreateNpcs")]
+        private static void NPCFactory_firstCreateNpcs_Postfix(NPCFactory __instance)
+        {
+            UselessDebuggerPrefix();
+            Debug.Log("MCSAllNPCsSpawn: calling NPCFactory_firstCreateNpcs_Postfix()");
+            Debug.Log(jsonData.instance.NPCLeiXingDate);
+        }
+
+        private static MCSAllNPCsSpawn Inst;
     }
 }
