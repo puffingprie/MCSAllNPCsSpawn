95f0f2aefe87371dc456e6a55c9b266bb43d1b20
puffingprie <morte1916@gmail.com>
2022-12-07 07:08:02 -0600

    add

--- MCSAllNPCsSpawn/MCSAllNPCsSpawn.cs
+++ MCSAllNPCsSpawn/MCSAllNPCsSpawn.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BepInEx;
 
 namespace MCSAllNPCsSpawn
 {
-    internal class MCSAllNPCsSpawn
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class MCSAllNPCsSpawn : BaseUnityPlugin
     {
+        public const string pluginGuid = "arx.mcs.allnpcsspawn";
+        public const string pluginName = "MCSAllNPCsSpawn";
+        public const string pluginVersion = "1.0.0.0";
+        void Start() { }
     }
 }
